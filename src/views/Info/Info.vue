e7a1ae019846abeb854546135a7c6a39d95f807f
jurayevcoder <davlatjurayev.1@gmail.com>
2023-06-15 03:16:34 +0500

    info change

--- src/views/Info/Info.vue
+++ src/views/Info/Info.vue
@@ -147,13 +147,107 @@
   </div>
 
   <div class="w-full mb-14 pb-14 border-b-2 border-[#414141] border-dashed">
+    <div class="flex items-center justify-center gap-3 mb-5">
+      <button
+        @click="btnShow"
+        :class="
+          show
+            ? 'text-blue-500 text-[20px] px-5 border-b border-blue-500'
+            : 'text-white text-[20px] px-5 border-b border-white hover:text-[#FF8C00]'
+        "
+      >
+        Show
+      </button>
+      <button
+        @click="btnAdd"
+        :class="
+          add
+            ? 'text-[#FF8C00] text-[20px] px-5 border-b border-[#FF8C00]'
+            : 'text-white text-[20px] px-5 border-b border-white hover:text-[#FF8C00]'
+        "
+      >
+        Add
+      </button>
+    </div>
+
     <div class="relative rounded-lg w-full bg-[#414141]">
       <div
-        class="flex justify-between items-center p-4 pl-10 sm:mb-5 bg-[#FF8C00] rounded-t-lg"
+        :class="show ? 'flex justify-between items-center p-4 pl-10 sm:mb-5 bg-blue-500 rounded-t-lg' : 'flex justify-between items-center p-4 pl-10 sm:mb-5 bg-[#FF8C00] rounded-t-lg'"
       >
         <h3 class="text-[24px] font-semibold text-white">Scill</h3>
       </div>
-      <form class="w-full p-5">
+
+      <div :class="show ? 'relative overflow-x-auto shadow-md sm:rounded-lg' : 'hidden'">
+      <table class="w-full text-sm text-left text-white">
+        <thead
+          class="text-[16px] text-white border-b-2 border-blue-500 uppercase bg-[#414141]"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Scill name</th>
+            <th scope="col" class="px-6 py-3">icon</th>
+            <th scope="col" class="px-6 py-3">
+              <span class="sr-only">Delete</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="bg-[#414141] border-b border-white border-dashed"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              JavaScript
+            </th>
+            <td class="px-6 py-4"><i class='bx bxl-javascript bx-tada text-[25px]'></i></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+          <tr
+            class="bg-[#414141] border-b border-white border-dashed"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              NodeJs
+            </th>
+            <td class="px-6 py-4"><i class='bx bxl-nodejs bx-tada text-[25px]'></i></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+          <tr class="bg-[#414141]">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              VueJs
+            </th>
+            <td class="px-6 py-4"><i class='bx bxl-vuejs bx-tada text-[25px]'></i></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+      <form :class="add? 'w-full p-5' : 'hidden'">
         <div class="w-full flex flex-col items-center gap-8 mb-20">
           <div class="w-full flex flex-col justify-between gap-8 lg:flex lg:flex-row">
             <div class="w-full lg:w-[50%]">
@@ -204,13 +298,74 @@
   </div>
 
   <div class="w-full mb-14 pb-14 border-b-2 border-[#414141] border-dashed">
+    <div class="flex items-center justify-center gap-3 mb-5">
+      <button
+        @click="btnWorkShow"
+        :class="
+          showWork
+            ? 'text-blue-500 text-[20px] px-5 border-b border-blue-500'
+            : 'text-white text-[20px] px-5 border-b border-white hover:text-[#FF8C00]'
+        "
+      >
+        Show
+      </button>
+      <button
+        @click="btnWorkAdd"
+        :class="
+          addWork
+            ? 'text-[#FF8C00] text-[20px] px-5 border-b border-[#FF8C00]'
+            : 'text-white text-[20px] px-5 border-b border-white hover:text-[#FF8C00]'
+        "
+      >
+        Add
+      </button>
+    </div>
+
+
     <div class="relative rounded-lg w-full bg-[#414141]">
       <div
-        class="flex justify-between items-center p-4 pl-10 sm:mb-5 bg-[#FF8C00] rounded-t-lg"
+        :class="showWork ? 'flex justify-between items-center p-4 pl-10 sm:mb-5 bg-blue-500 rounded-t-lg' : 'flex justify-between items-center p-4 pl-10 sm:mb-5 bg-[#FF8C00] rounded-t-lg'"
       >
         <h3 class="text-[24px] font-semibold text-white">Worked</h3>
       </div>
-      <form class="w-full p-5">
+
+      <div :class="showWork ? 'relative overflow-x-auto shadow-md sm:rounded-lg' : 'hidden'">
+      <table class="w-full text-sm text-left text-white">
+        <thead
+          class="text-[16px] text-white border-b-2 border-blue-500 uppercase bg-[#414141]"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Work name</th>
+            <th scope="col" class="px-6 py-3">Link</th>
+            <th scope="col" class="px-6 py-3">
+              <span class="sr-only">Delete</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="bg-[#414141]"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              Najot Ta'lim
+            </th>
+            <td class="px-6 py-4"><a href="https://najottalim.uz/" class="hover:underline hover:text-blue-500">https://najottalim.uz/</a></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+      <form :class="addWork ? 'w-full p-5' : 'hidden'">
         <div class="w-full flex flex-col items-center gap-8 mb-20">
           <div class="w-full flex flex-col justify-between gap-8 lg:flex lg:flex-row">
             <div class="w-full lg:w-[50%]">
@@ -261,13 +416,111 @@
   </div>
 
   <div class="w-full">
+    <div class="flex items-center justify-center gap-3 mb-5">
+      <button
+        @click="btnSocialShow"
+        :class="
+          showSocial
+            ? 'text-blue-500 text-[20px] px-5 border-b border-blue-500'
+            : 'text-white text-[20px] px-5 border-b border-white hover:text-[#FF8C00]'
+        "
+      >
+        Show
+      </button>
+      <button
+        @click="btnSocialAdd"
+        :class="
+          addSocial
+            ? 'text-[#FF8C00] text-[20px] px-5 border-b border-[#FF8C00]'
+            : 'text-white text-[20px] px-5 border-b border-white hover:text-[#FF8C00]'
+        "
+      >
+        Add
+      </button>
+    </div>
+
     <div class="relative rounded-lg w-full bg-[#414141]">
       <div
-        class="flex justify-between items-center p-4 pl-10 sm:mb-5 bg-[#FF8C00] rounded-t-lg"
+        :class="showSocial ? 'flex justify-between items-center p-4 pl-10 sm:mb-5 bg-blue-500 rounded-t-lg' : 'flex justify-between items-center p-4 pl-10 sm:mb-5 bg-[#FF8C00] rounded-t-lg'"
       >
         <h3 class="text-[24px] font-semibold text-white">Social media</h3>
       </div>
-      <form class="w-full p-5">
+
+      <div :class="showSocial ? 'relative overflow-x-auto shadow-md sm:rounded-lg' : 'hidden'">
+      <table class="w-full text-sm text-left text-white">
+        <thead
+          class="text-[16px] text-white border-b-2 border-blue-500 uppercase bg-[#414141]"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Social media name</th>
+            <th scope="col" class="px-6 py-3">Icon</th>
+            <th scope="col" class="px-6 py-3">Link</th>
+            <th scope="col" class="px-6 py-3">
+              <span class="sr-only">Delete</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="bg-[#414141] border-b border-white border-dashed"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              GitHub
+            </th>
+            <td class="px-6 py-4"><i class='bx bxl-github bx-tada text-[25px]' ></i></td>
+            <td class="px-6 py-4"><a href="https://github.com/jurayevcoder" class="hover:underline hover:text-blue-500">https://github.com/jurayevcoder</a></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+          <tr
+            class="bg-[#414141] border-b border-white border-dashed"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              LinkedIn
+            </th>
+            <td class="px-6 py-4"><i class='bx bxl-linkedin-square bx-tada text-[25px]' ></i></td>
+            <td class="px-6 py-4"><a href="https://www.linkedin.com/in/davlat-jo-rayev-305074275/" class="hover:underline hover:text-blue-500">https://www.linkedin.com/in/davlat-jo-rayev-305074275/</a></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+          <tr class="bg-[#414141]">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium whitespace-nowrap text-white"
+            >
+              Telegram
+            </th>
+            <td class="px-6 py-4"><i class='bx bxl-telegram bx-tada text-[25px]' ></i></td>
+            <td class="px-6 py-4"><a href="https://t.me/jurayevlive" class="hover:underline hover:text-blue-500">https://t.me/jurayevlive</a></td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-red-600 hover:underline"
+                >Delete</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+      <form :class="addSocial ? 'w-full p-5' : 'hidden'">
         <div class="w-full flex flex-col items-center gap-8 mb-20">
           <div class="w-full flex flex-col justify-between gap-8 lg:flex lg:flex-row">
             <div class="w-full lg:w-[50%]">
@@ -334,6 +587,26 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const show = ref(true);
+const add = ref(false);
+
+const showWork = ref(true);
+const addWork = ref(false);
+
+const showSocial = ref(true);
+const addSocial = ref(false);
+
+const btnShow = () => (show.value = true) && (add.value = false);
+const btnAdd = () => (add.value = true) && (show.value = false);
+
+const btnWorkShow = () => (showWork.value = true) && (addWork.value = false);
+const btnWorkAdd = () => (addWork.value = true) && (showWork.value = false);
+
+const btnSocialShow = () => (showSocial.value = true) && (addSocial.value = false);
+const btnSocialAdd = () => (addSocial.value = true) && (showSocial.value = false);
+</script>
 
 <style lang="scss" scoped></style>
